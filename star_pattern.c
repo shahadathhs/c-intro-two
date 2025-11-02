d8c3e72ea2235470c6f55ee0bf145e838ca836f7
shahadathhs <shahadathhossensajib732@gmail.com>
2025-11-02 10:29:11 +0600

    feat: star pattern reverse

--- star_pattern.c
+++ star_pattern.c
@@ -16,6 +16,15 @@ int main(void)
         
     }
     
+    for (int i = N - 1; i > 0; i--)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
 
     return 0;
 }
