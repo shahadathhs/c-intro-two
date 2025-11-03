6b131ea2a0444463ad6c5fce922edbe4a033490d
sajib-sv <sajib.softvence@gmail.com>
2025-11-03 20:32:43 +0600

    feat: selection sort

--- selection_sort.c
+++ selection_sort.c
@@ -2,6 +2,60 @@
 
 int main(void)
 {
-    
+     int N;
+
+    scanf("%d", &N);
+
+    int arr[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i; j < N; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+               int temp = arr[i];
+
+               arr[i] = arr[j];
+
+               arr[j] = temp;
+            }
+        }
+        
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i; j < N; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+               int temp = arr[i];
+
+               arr[i] = arr[j];
+
+               arr[j] = temp;
+            }
+        }
+        
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
